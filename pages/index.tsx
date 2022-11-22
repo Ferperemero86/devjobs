5fe6ce41caa3a728cdf3933099878793e6f47059
fernando.perez <fperez@wiley.com>
2022-11-22 16:56:26 +0000

    set apollo client

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,43 @@
 import { useEffect } from 'react';
 import styles from '../styles/Home.module.css';
+import { useQuery } from '@apollo/client';
+import withApollo from '../lib/withApollo';
+import { CHARACTERS_QUERY } from '../graphql/queries';
 
-export default function Home() {
-  return <h1>Home</h1>;
+interface Episode {
+  id: string;
+  episode: string;
+  air_date: string;
 }
+
+interface Origin {
+  dimension: string;
+  id: string;
+}
+
+interface Person {
+  episode: Episode[];
+  id: string;
+  image: string;
+  name: string;
+  gender: string;
+  origin: Origin;
+}
+
+function Home() {
+  const { data, loading } = useQuery(CHARACTERS_QUERY);
+
+  if (loading) return <p>Loading...</p>;
+
+  const jsData = JSON.parse(JSON.stringify(data));
+  console.log(jsData);
+  return (
+    <div>
+      {jsData.characters.results.map((person: Person) => (
+        <p>{person.name}</p>
+      ))}
+    </div>
+  );
+}
+
+export default withApollo(Home);
